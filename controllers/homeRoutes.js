633d8f70c7d00331cb36264e9758958a5a2cd040
Jacob Hargraves <jacobhargraves1@gmail.com>
2023-07-24 11:07:01 -0600

    login/signup view and route are added

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -48,4 +48,14 @@ router.get('/blogPosts/:id', async (req, res) => {
     }
   });
 
+  router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('/profile');
+      return;
+    }
+  
+    res.render('login');
+  });
+
 module.exports = router;
