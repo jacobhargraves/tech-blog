6d0bd4fb5d299a3478bf4d71c94b056c0bc52d33
Jacob Hargraves <jacobhargraves1@gmail.com>
2023-07-24 19:22:30 -0600

    dashboard page is added with route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { BlogPost, User } = require('../models');
-// const withAuth = require('../utils/auth');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
@@ -51,11 +51,31 @@ router.get('/blogPosts/:id', async (req, res) => {
   router.get('/login', (req, res) => {
     // If the user is already logged in, redirect the request to another route
     if (req.session.logged_in) {
-      res.redirect('/profile');
+      res.redirect('/dashboard');
       return;
     }
   
     res.render('login');
+  });
+
+  // Use withAuth middleware to prevent access to route
+router.get('/dashboard', withAuth, async (req, res) => {
+    try {
+      // Find the logged in user based on the session ID
+      const userData = await User.findByPk(req.session.user_id, {
+        attributes: { exclude: ['password'] },
+        include: [{ model: BlogPost }],
+      });
+  
+      const user = userData.get({ plain: true });
+  
+      res.render('dashboard', {
+        ...user,
+        logged_in: true
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
   });
 
 module.exports = router;
