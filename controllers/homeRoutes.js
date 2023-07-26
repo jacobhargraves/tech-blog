592da00933cfff32654725f41b47b36721daae10
Jacob Hargraves <jacobhargraves1@gmail.com>
2023-07-26 07:31:46 -0600

    added log in requirements for viewing articles

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -27,7 +27,7 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/blogPosts/:id', async (req, res) => {
+router.get('/blogPosts/:id', withAuth, async (req, res) => {
     try {
       const blogPostData = await BlogPost.findByPk(req.params.id, {
         include: [
@@ -41,7 +41,8 @@ router.get('/blogPosts/:id', async (req, res) => {
       const blogPost = blogPostData.get({ plain: true });
   
       res.render('blogPost', {
-        ...blogPost
+        ...blogPost,
+        logged_in: req.session.logged_in
       });
     } catch (err) {
       res.status(500).json(err);
