4a48329f363de8fa15072c72efd0fb02e10eaeb7
Jacob Hargraves <jacobhargraves1@gmail.com>
2023-07-26 07:49:47 -0600

    added comment model and logic in index file

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const BlogPost = require('./BlogPost');
+const Comment = require('./Comment');
 
 User.hasMany(BlogPost, {
   foreignKey: 'user_id',
@@ -8,6 +9,24 @@ User.hasMany(BlogPost, {
 
 BlogPost.belongsTo(User, {
   foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(BlogPost, {
+  foreignKey: 'blogPost_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+BlogPost.hasMany(Comment, {
+  foreignKey: 'blogPost_id',
+  onDelete: 'CASCADE'
 });
 
 module.exports = { User, BlogPost };
