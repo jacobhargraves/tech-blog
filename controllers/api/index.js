5e3a96d2b0dee67315c6e7642ef6254e188274dd
Jacob Hargraves <jacobhargraves1@gmail.com>
2023-07-26 10:31:00 -0600

    more comment route removal

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const blogPostRoutes = require('./blogPostRoutes.js');
-const commentRoutes = require('./commentRoutes.js');
 
 router.use('/users', userRoutes);
 router.use('/blogPosts', blogPostRoutes);
